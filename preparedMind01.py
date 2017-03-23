68126bfd6455db0ff00e592d4ee280ea2da23f2f
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-03-23 00:44:54 -0400

    commit

---
+++ preparedMind01.py
@@ -0,0 +1,19 @@
+Chris = raw_input("Are you Chris from PREPARED MIND 101? 1 - yes, 2 - no: ")
+if Chris == "1":
+    print("")
+    print("*****************************************************************")
+    print("* This is Chris from PREPARED MIND 101. I'm ready for anything. *")
+    print("*****************************************************************")
+    print("")
+elif Chris == "2":
+    print("")
+    print("****************")
+    print("* Got a match? *")
+    print("****************")
+    print("")
+else:
+    print("")
+    print("*************")
+    print("* Confused? *")
+    print("*************")
+    print("")
